d7451d43e4109d41e846565b707d9dfcfb424f06
Klaivert <69586612+klaivertt@users.noreply.github.com>
2025-01-24 14:12:24 +0100

    collision update

--- Game/Collision.c
+++ Game/Collision.c
@@ -14,15 +14,63 @@ sfBool CollisionRectRect(sfIntRect* const _rect1, sfIntRect* const _rect2)
     
 }
 
-//sfBool CollisionPointRect(sfIntRect* const _rect1, sfVector2i const _point)
-//{
-//	if (sfFloatRect_contains(_rect1, (float)_point.x, (float)_point.x))
-//	{
-//		return sfTrue;
-//	}
-//	else
-//	{
-//		return sfFalse;
-//	}
-//    
-//}
+sfBool CollisionPointRect(sfIntRect* const _rect1, sfVector2i const _point)
+{
+	if (sfFloatRect_contains(_rect1, (float)_point.x, (float)_point.x))
+	{
+		return sfTrue;
+	}
+	else
+	{
+		return sfFalse;
+	}
+    
+}
+
+sfBool ColisionCircleCircle(sfVector2f _center1, float _radius1, sfVector2f _center2, float _radius2)
+{
+	float distance = sqrtf(powf(_center2.x - _center1.x, 2) + powf(_center2.y - _center1.y, 2));
+	if (distance < _radius1 + _radius2)
+	{
+		return sfTrue;
+	}
+	else
+	{
+		return sfFalse;
+	}
+	
+}
+
+sfBool CollisionCircleRect(sfVector2f _center, float _radius, sfIntRect* const _rect)
+{
+	sfVector2f closestPoint = { 0 };
+	closestPoint.x = fmaxf(_rect->left, fminf(_center.x, _rect->left + _rect->width));
+	closestPoint.y = fmaxf(_rect->top, fminf(_center.y, _rect->top + _rect->height));
+
+	float distance = sqrtf(powf(_center.x - closestPoint.x, 2) + powf(_center.y - closestPoint.y, 2));
+	if (distance < _radius)
+	{
+		return sfTrue;
+	}
+	else
+	{
+		return sfFalse;
+	}
+	
+}
+
+sfBool CollisionPointCircle(sfVector2f _point, sfVector2f _center, float _radius)
+{
+	float distance = sqrtf(powf(_point.x - _center.x, 2) + powf(_point.y - _center.y, 2));
+	if (distance < _radius)
+	{
+		return sfTrue;
+	}
+	else
+	{
+		return sfFalse;
+	}
+	
+}
+
+
--- Game/Collision.h
+++ Game/Collision.h
@@ -3,9 +3,43 @@
 
 #include "Common.h"
 
-sfBool CollisionRectRect(sfIntRect* const, sfIntRect* const);
+// * @brief Checks for collision between two rectangles.
+// *
+// * This function checks if two rectangles intersect.
+// *
+// * @param _rect1 Pointer to the first rectangle.
+// * @param _rect2 Pointer to the second rectangle.
+// * @return sfBool True if the rectangles intersect, false otherwise.
+sfBool CollisionRectRect(sfIntRect* const _rect1, sfIntRect* const _rect2);
 
-//sfBool CollisionPointRect(sfIntRect* const _rect1, sfVector2i const _point);
+// * @brief Checks for collision between a point and a rectangle.
+// *
+// * This function checks if a point is inside a rectangle.
+// *
+// * @param _rect1 Pointer to the rectangle.
+// * @param _point The point to check.
+// * @return sfBool True if the point is inside the rectangle, false otherwise.
+sfBool CollisionPointRect(sfIntRect* const _rect1, sfVector2i const _point);
 
+// * @brief Checks for collision between two circles.
+// *
+// * This function checks if two circles intersect.
+// *
+// * @param _center1 The center of the first circle.
+// * @param _radius1 The radius of the first circle.
+// * @param _center2 The center of the second circle.
+// * @param _radius2 The radius of the second circle.
+// * @return sfBool True if the circles intersect, false otherwise.
+sfBool ColisionCircleCircle(sfVector2f _center1, float _radius1, sfVector2f _center2, float _radius2);
+
+// * @brief Checks for collision between a circle and a rectangle.
+// *
+// * This function checks if a circle intersects a rectangle.
+// *
+// * @param _center The center of the circle.
+// * @param _radius The radius of the circle.
+// * @param _rect Pointer to the rectangle.
+// * @return sfBool True if the circle intersects the rectangle, false otherwise.
+sfBool CollisionCircleRect(sfVector2f _center, float _radius, sfIntRect* const _rect);
 #endif
 
