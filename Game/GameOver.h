f51f7122604d86bdb9ecfb676196bc838f5c74e3
Klaivert <69586612+klaivertt@users.noreply.github.com>
2025-01-24 14:18:23 +0100

    comment

--- Game/GameOver.h
+++ Game/GameOver.h
@@ -12,8 +12,13 @@ typedef struct GameOverData
 	sfSprite* sprite;
 }GameOverData;
 
+//* @brief Function to load the Game Over screen resources.
 void LoadGameOver(void);
+
+//* @brief Function to handle key pressed events in the Game Over screen.
+//* @param _key The key that was pressed.
 void KeyPressedGameOver(sfKeyEvent _key);
+
 void UpdateGameOver(sfRenderWindow* const _renderWindow, float _dt);
 void DrawGameOver(sfRenderWindow* const _renderWindow);
 void CleanupGameOver(void);
--- Game/GameState.h
+++ Game/GameState.h
@@ -14,7 +14,14 @@ typedef enum GameState
 }GameState;
 #pragma endregion
 
+//* @brief Function to set the current game state.
+//* This function sets the current game state to the one passed as a parameter. 
+//* And calls the corresponding function to load the resources for that state and clean up the resources of the previous state.
+//* @param _gameState The game state to set.
 void SetGameState(GameState _gameState);
+
+//* @brief Function to get the current game state.
+//* This function returns the current game state.
 GameState GetGameState(void);
 
 #endif // !GAME_STATE_H
